e117b1c6f3e161ac27b2c5004792128835717e92
HuangChuang <1125677925@qq.com>
2025-09-29 08:31:40 +0800

    修改日频数据采集时间

--- stock_data_service/settings.py
+++ stock_data_service/settings.py
@@ -241,7 +241,7 @@ CRONJOBS = [
 
     # 个股数据 
     ('59 1,12,20 * * *', 'scheduled_tasks.stock_data_query_tasks.individual_stock_tasks.fetch_individual_stocks', f'>> {BASE_DIR}/logs/fetch_individual_stock_list.log 2>&1'), 
-    ('40 12,22 * * *', 'scheduled_tasks.stock_data_query_tasks.individual_stock_tasks.update_individual_stock_daily_data', f'>> {BASE_DIR}/logs/update_individual_stock_daily_data.log 2>&1'),
+    ('40 16,23 * * *', 'scheduled_tasks.stock_data_query_tasks.individual_stock_tasks.update_individual_stock_daily_data', f'>> {BASE_DIR}/logs/update_individual_stock_daily_data.log 2>&1'),
 
     # 大盘数据
 ]
