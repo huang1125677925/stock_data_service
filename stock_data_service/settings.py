4173a55769a7de2c9af2784ad3024f5960034320
HuangChuang <1125677925@qq.com>
2025-10-21 08:30:14 +0800

    增加大盘数据抓取

--- stock_data_service/settings.py
+++ stock_data_service/settings.py
@@ -248,6 +248,7 @@ CRONJOBS = [
     # 个股数据 
     ('59 1,12,20 * * *', 'scheduled_tasks.stock_data_query_tasks.individual_stock_tasks.fetch_individual_stocks', f'>> {BASE_DIR}/logs/fetch_individual_stock_list.log 2>&1'), 
     ('40 16,23 * * *', 'scheduled_tasks.stock_data_query_tasks.individual_stock_tasks.update_individual_stock_daily_data', f'>> {BASE_DIR}/logs/update_individual_stock_daily_data.log 2>&1'),
+    ('20 18 * * *', 'scheduled_tasks.stock_data_query_tasks.individual_stock_tasks.update_index_stock_daily_data', f'>> {BASE_DIR}/logs/update_index_stock_daily_data.log 2>&1'),
 ]
 
 # Crontab配置
